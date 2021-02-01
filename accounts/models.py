1604eb15240bc73c263e1bac188fb75284c7eaca
Andrew Smarty <smartyleey@gmail.com>
2021-02-01 17:47:33 +0100

    file saving bug fixed

--- accounts/models.py
+++ accounts/models.py
@@ -51,13 +51,13 @@ class Profile(models.Model):
             otp = generate_otp_number()
             self.otp = otp
         
+        return super(Profile, self).save(*args, **kwargs)
         # for image
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-        return super(Profile, self).save(*args, **kwargs)
     
     def delete(self, *args, **kwargs):
         self.image.delete()
